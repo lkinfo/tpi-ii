22b1fea0a803b6670adafec0b60b61d99458493e
lkinfo <thaynaralyz@gmail.com>
2018-08-20 20:11:33 -0300

    Mensagem - exemplo: Aula 2

---
+++ init.sh
@@ -0,0 +1,5 @@
+git clone git remote add origin https://github.com/daleonrd/tpi-ii.git
+git config --global user.email "thaynaralyz@gmail.com"
+git config --global user.name "lkinfo"
+cd tpi-ii
+code .
